a959e849ae860374372527dbfdf6daa8b8fc3122
Katya Belova <katrin.belova@gmail.com>
2018-03-20 23:22:56 +0200

    Header with bootstrap grid

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
 const $ = window.$ = window.jQuery = require('jquery');
+import './assets/scripts/slick.min.js';
+
+$(document).ready(function(){
+  $('.mainCarousel').slick({
+    centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
+});
