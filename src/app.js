bff0d4e55c453fa4bd72994d0e2bd1ace20711ec
Katya Belova <katrin.belova@gmail.com>
2018-03-21 18:13:25 +0200

    Section slider. to be continued.

--- src/app.js
+++ src/app.js
@@ -1,30 +1,4 @@
 const $ = window.$ = window.jQuery = require('jquery');
 import './assets/scripts/slick.min.js';
+import 'components/main-carousel/';
 
-$(document).ready(function(){
-  $('.mainCarousel').slick({
-    centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-  });
-});
